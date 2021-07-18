87fddce8e258f05995b6a200efd0ff611156f962
seacrest <cassiep1986@gmail.com>
2021-07-18 14:07:57 -0700

    started add models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,21 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+   id: {
+     type: DataTypes.INTEGER,
+     allownull: false,
+     primaryKey: true, 
+     autoIncrement: true
+   },
+   product_name: {
+     type:Datatype.STRING,
+     allownull: false
+   }
+   price: {
+     type: Datatype.DECIMAL,
+     allownull: false, 
+     
+   }
   },
   {
     sequelize,
