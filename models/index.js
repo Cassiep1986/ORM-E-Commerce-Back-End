2e043e154fab56b42ae4eea56e6d78b997092495
seacrest <cassiep1986@gmail.com>
2021-07-19 16:48:16 -0700

    added associations

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsto(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongstoMany(Tag, {
+  foreignKey: "tag_id",
+  onDelete: 'CASCADE',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongstoMany(Product, {
+  foreignKey: "product_id",
+  onDelete: 'CASCADE',
+});
 module.exports = {
   Product,
   Category,
